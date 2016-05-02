b50d1732a0d3a69843c5029650104df76a26b328
retwedt <retwedt@gmail.com>
2016-05-02 10:13:19 -0500

    move fade-onscroll effect to main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@
 
 
 // get dom elements
-var mainHeader = document.querySelector(".slide-show");
+var header = document.querySelector(".slide-show") || document.querySelector(".scroll-header") || document.querySelector(".blank-header");
+
 
 // when user has scrolled a certain distance, desaturate header
 document.onscroll = function() {
 	if (document.body.scrollTop <= 100) {
-		mainHeader.setAttribute("class", "slide-show saturate");
+		header.classList.add("saturate");
+		header.classList.remove("desaturate");
 	} else {
-		mainHeader.setAttribute("class", "slide-show desaturate");
+		header.classList.add("desaturate");
+		header.classList.remove("saturate");
 	}
 }
--- js/scroll.js
+++ js/scroll.js
@@ -1,83 +1,82 @@
 //main.js
 
 
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+// MIT license
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
+
 // get dom elements
-var showHeader = document.querySelector(".show-header");
-var img = showHeader.querySelector("img");
 var rows = document.querySelectorAll(".row");
 var cells = document.querySelectorAll(".cell");
 //Global ID of mouse down interval
 var mousedownID = -1;
 
 
-// get current screen dimensions, update them when the window is resized
-var winWidth = 0;
-var winHeight = 0;
-var headerHeight = 276; // this is set by default
-var mediaRatio = img.width / img.height; // variable to calculate header image aspect ratio
-window.onload = getDimensions;
-window.onresize = getDimensions;
-
-
-// get dimensions of page, and calculate appropriate 
-// size for header image
-function getDimensions() {
-	winWidth = document.documentElement.clientWidth;
-	winHeight = document.documentElement.clientHeight;
-
-	var tempWidth = winWidth;
-	var tempHeight = tempWidth * (1/mediaRatio);
-	if (tempHeight < headerHeight) {
-		tempHeight = headerHeight;
-		tempWidth = tempHeight * mediaRatio;
-	}
-	img.style.width = tempWidth + "px";
-	img.style.height = tempHeight + "px";
-}
-
-
-
-
-
-
-// when user has scrolled a certain distance, desaturate header
-document.onscroll = function() {
-	if (document.body.scrollTop <= 100) {
-		showHeader.setAttribute("class", "show-header saturate");
-	} else {
-		showHeader.setAttribute("class", "show-header desaturate");
-	}
-}
-
 
 // setup row elements
 for (var i = 0; i < rows.length; i++) {
+	var rowScrollWrap = rows[i].querySelector(".row-scroll-wrap");
 	// access the row-scroll element within each row
-	var rowScroll = rows[i].querySelector(".row-scroll");
+	var rowScroll = rowScrollWrap.querySelector(".row-scroll");
 	// access the cells in each row-scroll element
 	var currentCells = rowScroll.querySelectorAll(".cell");
 	// set the width of the row-scroll element based on the number of cells
 	var width = (currentCells.length * 254) + 24;
 	rowScroll.style.width = width + "px";
 
+	// rowScrollWrap.addEventListener("scroll", fixScroll.debounce(adjustScroll, 500));
+
 	// add events to left and right buttons
-	// from https://stackoverflow.com/questions/15505272/javascript-while-mousedown
 	var rightBtn = rows[i].querySelector(".row-right");
 	var leftBtn = rows[i].querySelector(".row-left");
-	leftBtn.addEventListener("mousedown", function() { mouseDown(this); });
-	rightBtn.addEventListener("mousedown", function() { mouseDown(this); });
+	// from https://stackoverflow.com/questions/15505272/javascript-while-mousedown
+	leftBtn.addEventListener("mousedown", mouseDown);
+	rightBtn.addEventListener("mousedown", mouseDown);
 	leftBtn.addEventListener("mouseup", mouseUp);
 	rightBtn.addEventListener("mouseup", mouseUp);
 	leftBtn.addEventListener("mouseout", mouseUp);
 	rightBtn.addEventListener("mouseout", mouseUp);
 }
-function mouseDown(el) {
+
+
+// when the mouse is down, use setInterval to run the scroll function every 75 ms
+function mouseDown() {
+	var self = this; // reference for passing to setInterval
   if(mousedownID==-1) {
     mousedownID = setInterval(function() {
-    	scroll(el)
-    }, 75 /*ms*/);
+    	scroll(self);
+    }, 75);
   }
 }
+// when the mouse is up, use clearInterval to stop the scroll function 
 function mouseUp() {
   if(mousedownID!=-1) {
     clearInterval(mousedownID);
@@ -109,7 +108,7 @@ for (var j=0; j<cells.length; j++) {
 			if (this.classList.contains("b-on")) {
 				this.setAttribute("class", "cell-btn b-off");
 				cellText.style.transform = "translate(0%, 100%)";
-				this.parentElement.querySelector("img").setAttribute("class", "");
+				this.parentElement.querySelector("img").setAttribute("class", "un-dim");
 			} else {
 				this.setAttribute("class", "cell-btn b-on");
 				cellText.style.transform = "translate(0%, 0%)";
